d210d03da8f5a079ea79d16e8ddd92727b9264c9
Efsane2003 <eabbasova145@gmail.com>
2023-04-30 12:03:40 +0400

    Simple Calculator

--- calculate/main.js
+++ calculate/main.js
@@ -1,60 +1,51 @@
-let value1=document.querySelector(".value1")
-let value2=document.querySelector(".value2")
-let result=document.querySelector("#result")
-let addition=document.querySelector(".addition")
-let substract=document.querySelector(".substract")
-let multiply=document.querySelector(".multiply")
-let division=document.querySelector(".division")
-let reset=document.querySelector(".reset")
+let value1 = document.querySelector(".value1");
+let value2 = document.querySelector(".value2");
+let result = document.querySelector("#result");
+let addition = document.querySelector(".addition");
+let substract = document.querySelector(".substract");
+let multiply = document.querySelector(".multiply");
+let division = document.querySelector(".division");
+let reset = document.querySelector(".reset");
 
+let bool = false;
+addition.addEventListener("click", function () {
+  if (value1.value == "" || value2.value == "") {
+    alert("valua are not null!");
+  } else {
+    let sum = +value1.value + +value2.value;
+    result.innerText = sum;
+  }
+});
 
+substract.addEventListener("click", function () {
+  if (value1.value == "" || value2.value == "") {
+    alert("valua are not null!");
+  } else {
+    let substr = +value1.value - +value2.value;
+    result.innerText = substr;
+  }
+});
 
+multiply.addEventListener("click", function () {
+  if (value1.value == "" || value2.value == "") {
+    alert("valua are not null!");
+  } else {
+    let multi = +value1.value * +value2.value;
+    result.innerText = multi;
+  }
+});
 
-addition.addEventListener("click",function(){
-    value1.addEventListener("input",function(event){
-    event.target.value
-})
-value2.addEventListener("input",function(event){
-    event.target.value
-})
-result.value=+value1.value+ +value2.value
-})
+division.addEventListener("click", function () {
+  if (value1.value == "" || value2.value == "") {
+    alert("valua are not null!");
+  } else {
+    let divi = +value1.value / +value2.value;
+    result.innerText = divi;
+  }
+});
 
-
-substract.addEventListener("click",function(){
-    value1.addEventListener("input",function(event){
-        event.target.value
-    })
-    value2.addEventListener("input",function(event){
-        event.target.value
-    })
-    result.value=value1.value-value2.value
-})
-
-multiply.addEventListener("click",function(){
-    value1.addEventListener("input",function(event){
-        event.target.value
-    })
-    value2.addEventListener("input",function(event){
-        event.target.value
-    })
-    result.value=value1.value*value2.value
-})
-
-division.addEventListener("click",function(){
-    value1.addEventListener("input",function(event){
-        event.target.value
-    })
-    value2.addEventListener("input",function(event){
-        event.target.value
-    })
-    result.value=value1.value/value2.value
-})
-
-reset.addEventListener("click",function(){
-    value1.value="" 
-    value2.value=""
-    result.value=""
-})
-
-
+reset.addEventListener("click", function () {
+  value1.value = "";
+  value2.value = "";
+  result.innerText = 0;
+});
