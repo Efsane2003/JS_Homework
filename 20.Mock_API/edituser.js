7df20c678be1087bb0414dbc380277871cfbf5a2
Efsane2003 <eabbasova145@gmail.com>
2023-05-11 11:37:32 +0400

    CRUD

--- 20.Mock_API/edituser.js
+++ 20.Mock_API/edituser.js
@@ -2,44 +2,61 @@ let myform=document.querySelector("#myform")
 let inputName=document.querySelector("#exampleInput")
 let inputEmail=document.querySelector("#exampleInpuEmail")
 
-let row = document.querySelector(".row");
+// let row = document.querySelector(".row");
 
-async function listAllUsers() {
-    let response = await fetch("http://localhost:8080/users");
-    let data = await response.json();
-    // console.log(data)
-    data.forEach((obj) => {
-      row.innerHTML += `
-         <div class="col col-5">
-                  <div class="card ms-4" style="width: 18rem;">
-                      <div class="card-body">
-                        <h5 class="card-title">${obj.fristName}</h5>
-                        <p class="card-text">${obj.email}</p>
-                        <i class="fa-solid fa-trash"></i>
-                        <i class="fa-solid fa-pencil"></i>
-                      </div>
-                      </div>
-                    </div>
-          `;
-    });
-  }
+// async function listAllUsers() {
+//     let response = await fetch("http://localhost:8080/users");
+//     let data = await response.json();
+//     // console.log(data)
+//     data.forEach((obj) => {
+//       row.innerHTML += `
+//          <div class="col col-5">
+//                   <div class="card ms-4" style="width: 18rem;">
+//                       <div class="card-body">
+//                         <h5 class="card-title">${obj.fristName}</h5>
+//                         <p class="card-text">${obj.email}</p>
+//                         <i class="fa-solid fa-trash"></i>
+//                         <i class="fa-solid fa-pencil"></i>
+//                       </div>
+//                       </div>
+//                     </div>
+//           `;
+//     });
+//   }
 
 
 
-myform.addEventListener("submit",function(e){
+// myform.addEventListener("submit",function(e){
+//     e.preventDefault()
+//     let obj={
+//         fristName:inputName.value,
+//         email:inputEmail.value
+//     }
+//     fetch("http://localhost:8080/users",{
+//         method:"PUT",
+//         headers:{
+//             "Content-type":"application/json",
+//         },
+//         body:JSON.stringify(obj)
+//     }).then(()=>{
+//         listAllUsers()
+//     })
+//     window.location.href="http://127.0.0.1:5501/20.Mock_API/index.html"
+// } )
+
+let id=new URLSearchParams(window.location.search).get("id")
+console.log(id)
+axios(`http://localhost:8080/users/${id}`)
+.then((res)=>{
+  inputName.value=res.data.fristName;
+  inputEmail.value=res.data.email
+  myform.addEventListener("submit",function(e){
     e.preventDefault()
-    let obj={
-        fristName:inputName.value,
-        email:inputEmail.value
+    let editUser={
+      fristName:inputName.value,
+      email:inputEmail.value
     }
-    fetch("http://localhost:8080/users",{
-        method:"PUT",
-        headers:{
-            "Content-type":"application/json",
-        },
-        body:JSON.stringify(obj)
-    }).then(()=>{
-        listAllUsers()
-    })
-    window.location.href="http://127.0.0.1:5501/20.Mock_API/index.html"
-} )
+    axios.patch(`http://localhost:8080/users/${id}`,editUser)
+    window.location="index.html"
+  })
+})
--- 20.Mock_API/script.js
+++ 20.Mock_API/script.js
@@ -10,17 +10,16 @@ addBtn.addEventListener("click", function () {
 });
 
 
-
-
-
-
 // card
 
 async function listAllUsers() {
+
+  row.innerHTML=""
   let response = await fetch("http://localhost:8080/users");
   let data = await response.json();
   // console.log(data)
   data.forEach((obj) => {
+    
     row.innerHTML += `
        <div class="col col-5">
                 <div class="card ms-4" style="width: 18rem;">
@@ -28,7 +27,7 @@ async function listAllUsers() {
                       <h5 class="card-title">${obj.fristName}</h5>
                       <p class="card-text">${obj.email}</p>
                       <i class="fa-solid fa-trash" id="${obj.id}" onclick=deleteById(${obj.id})></i>
-                      <i class="fa-solid fa-pencil" id="${obj.id}" onclick=editById(${obj.id}) ></i>
+                      <a href="edituser.html?id=${obj.id}"  class="btn btn-black"><i class="fa-solid fa-pencil"></i></a>
                     </div>
                     </div>
                     </div>
@@ -42,34 +41,36 @@ async function deleteById(id){
     await fetch(`http://localhost:8080/users/${id}`,{
         method:"DELETE"
     })
-    console.log(id)
+    // console.log(id)
     listAllUsers()
 }
+// deleteById()
 
 // edit icon 
 function editById(id){
     console.log(id)
     window.location.href="http://127.0.0.1:5501/20.Mock_API/edituser.html"
-    // myform.addEventListener("submit",function(e){
-    //     e.preventDefault()
-    //     let obj={
-    //         fristName:inputName.value,
-    //         email:inputEmail.value
-    //     }
-    //     fetch("http://localhost:8080/users",{
-    //         method:"PUT",
-    //         headers:{
-    //             "Content-type":"application/json",
-    //         },
-    //         body:JSON.stringify(obj)
-    //     }).then(()=>{
-    //         listAllUsers()
-    //     })
+    myform.addEventListener("submit",function(e){
+        e.preventDefault()
+        let obj={
+            fristName:inputName.value,
+            email:inputEmail.value
+        }
+        fetch("http://localhost:8080/users",{
+            method:"PUT",
+            headers:{
+                "Content-type":"application/json",
+            },
+            body:JSON.stringify(obj)
+        }).then(()=>{
+            listAllUsers()
+        })
         
-    // } )
+    } )
 }
 
 
+    
 
 
 
